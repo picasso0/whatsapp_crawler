b2bf3c97b10c8a728c1a38fc4aa4da37a6e627cf
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-05-13 23:10:39 +0330

    send token in c2 to worker

--- c2/utils.py
+++ c2/utils.py
@@ -7,8 +7,8 @@ from os import getenv
 def send_profiles_to_worker(worker_ip, profiles_json):
     load_dotenv()
     worker_port = int(getenv("WORKER_PORT"))
-    headers = {"Content-Type": "application/json"}
+    token = str(getenv("TOKEN"))
+    headers = {"Content-Type": "application/json", "authorization": token}
     response = post(
         f"http://{worker_ip}:{worker_port}/check_numbers/", data=profiles_json, headers=headers)
-
     return response.json()
