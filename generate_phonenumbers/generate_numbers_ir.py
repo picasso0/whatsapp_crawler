052dd168a7c139bbe97b357e79bad769d491f835
Your Name <you@example.com>
2024-05-15 03:03:01 -0400

    generate iran numbers

--- generate_phonenumbers/generate_numbers_ir.py
+++ generate_phonenumbers/generate_numbers_ir.py
@@ -1,42 +1,42 @@
-import itertools
-import motor.motor_asyncio
+# import itertools
+# import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
-db = client.gathering
+# client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
+# db = client.gathering
 
-# Define the prefix for Israeli mobile numbers
-prefix = "9725"
+# # Define the prefix for Israeli mobile numbers
+# prefix = "9725"
 
-# Define the length of the remaining digits
-length = 8
+# # Define the length of the remaining digits
+# length = 8
 
-# Generate all possible combinations of the remaining digits
-digits = [str(i) for i in range(10)]
-combinations = [''.join(combo) for combo in itertools.product(digits, repeat=length)]
+# # Generate all possible combinations of the remaining digits
+# digits = [str(i) for i in range(10)]
+# combinations = [''.join(combo) for combo in itertools.product(digits, repeat=length)]
 
-# Combine the prefix and the remaining digits to form the complete phone numbers
-phone_numbers = [prefix + combo for combo in combinations]
-# Print the generated phone numbers
-for number in phone_numbers:
-    db.profile.insert_one({"mobile":number})
+# # Combine the prefix and the remaining digits to form the complete phone numbers
+# phone_numbers = [prefix + combo for combo in combinations]
+# # Print the generated phone numbers
+# for number in phone_numbers:
+#     db.profile.insert_one({"mobile":number})
 
 
 
 # FOR IRAN
 
-# import random
+import random
 
 
 
 
-# client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
-# db = client.gathering
-# # Define the format of the Iranian mobile numbers
-# mobile_format = "98919{:07d}"
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
+db = client.gathering
+# Define the format of the Iranian mobile numbers
+mobile_format = "98919{:07d}"
 
-# # Generate 100 random phone numbers
-# phone_numbers = [mobile_format.format(random.randint(0, 9999999)) for _ in range(1000)]
-# breakpoint()
-# # Print the phone numbers
-# for phone_number in phone_numbers:
-#     db.profile.insert_one({"mobile":phone_number})
+# Generate 100 random phone numbers
+phone_numbers = [mobile_format.format(random.randint(0, 9999999)) for _ in range(1000)]
+breakpoint()
+# Print the phone numbers
+for phone_number in phone_numbers:
+    db.profile.insert_one({"mobile":phone_number})
