48f70c72ee49a47ec3522a8f094b98a835058018
picasso-mac <90257625+picasso0@users.noreply.github.com>
2024-05-18 23:49:47 +0330

    fix

--- generate_phonenumbers/generate_numbers_ir.py
+++ generate_phonenumbers/generate_numbers_ir.py
@@ -4,21 +4,22 @@ import motor.motor_asyncio
 client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
 db = client.gathering
 
+def generate_israeli_mobile_numbers(prefix, length):
+    digits = [str(i) for i in range(10)]
+    
+    for combo in itertools.product(digits, repeat=length):
+        number = prefix + ''.join(combo)
+        yield number
+
 # Define the prefix for Israeli mobile numbers
 prefix = "9725"
-
 # Define the length of the remaining digits
 length = 8
 
-# Generate all possible combinations of the remaining digits
-digits = [str(i) for i in range(10)]
-combinations = [''.join(combo) for combo in itertools.product(digits, repeat=length)]
-
-# Combine the prefix and the remaining digits to form the complete phone numbers
-phone_numbers = [prefix + combo for combo in combinations]
-# Print the generated phone numbers
-for number in phone_numbers:
-    db.profile.insert_one({"mobile":number,"whatsapp_searching":0})
+# Generate and print all Israeli mobile phone numbers
+for mobile_number in generate_israeli_mobile_numbers(prefix, length):
+    db.profile.insert_one({"mobile":mobile_number,"whatsapp_searching":0})
+    
 
 
 
