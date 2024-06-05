3756367138ba96823d65418e0861b6e241fbcd00
Your Name <you@example.com>
2024-06-05 10:09:17 -0400

    fix generate_numbers

--- generate_phonenumbers/generate_numbers_ir.py
+++ generate_phonenumbers/generate_numbers_ir.py
@@ -1,54 +1,50 @@
+import phonenumbers
 import motor.motor_asyncio
 import asyncio
+from phonenumbers import PhoneNumberType
 
-async def generate_numbers(prefix, length):
-    digits = [str(i) for i in range(10)]
-    
-    async def generate_combinations(current_combo=""):
-        if len(current_combo) == length:
-            yield prefix + current_combo
+def is_israeli_mobile_number(number):
+    if phonenumbers.is_valid_number(number):
+        number_type = phonenumbers.number_type(number)
+        if number_type == PhoneNumberType.MOBILE:
+            country_code = number.country_code
+            if country_code == 972:  # Israel country code
+                return True
+    return False
+
+async def generate_and_insert_numbers(prefix, collection, start=10000000, end=100000000):
+    for i in range(start, end):
+        number_str = f"+972{prefix}{i:07d}"
+        number = phonenumbers.parse(number_str, "IL")
+        print("_____________________")
+        print(number)
+        if is_israeli_mobile_number(number):
+            formatted_number = phonenumbers.format_number(number, phonenumbers.PhoneNumberFormat.E164)
+            existing_number = await collection.find_one({"mobile": formatted_number})
+            if not existing_number:
+                await collection.insert_one({"mobile": formatted_number, "whatsapp_searching": 0})
+                print(f"{formatted_number} inserted")
+            else:
+                print(f"{formatted_number} is existed")
         else:
-            for digit in digits:
-                async for number in generate_combinations(current_combo + digit):
-                    yield number
-    
-    async for number in generate_combinations():
-        yield number
-
-async def insert_numbers(numbers, db):
-    async for number in numbers:
-        # Check if the document exists
-        existing_document = db.profile.find_one({"mobile": number})
-        if not existing_document:
-            await db.profile.insert_one({"mobile": number, "whatsapp_searching": 0})
+            print("not il number")
+            
 
 async def main():
     client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
     db = client["gathering"]
-    prefix = "9725"
-    length = 8
+    collection = db["profile"]
 
-    numbers = generate_numbers(prefix, length)
-    await insert_numbers(numbers, db)
+    mobile_prefixes = ["050", "051", "052", "053", "054", "055", "056", "057", "058", "059"]
+    tasks = []
+
+    for prefix in mobile_prefixes:
+        task = asyncio.create_task(generate_and_insert_numbers(prefix, collection))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+    client.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# FOR IRAN
-
-# import random
-
-
-
-
-# client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://77.238.108.86:27000/log?retryWrites=true&w=majority")
-# db = client.gathering
-# # Define the format of the Iranian mobile numbers
-# mobile_format = "98919{:07d}"
-
-# # Generate 100 random phone numbers
-# phone_numbers = [mobile_format.format(random.randint(0, 9999999)) for _ in range(1000)]
-# breakpoint()
-# # Print the phone numbers
-# for phone_number in phone_numbers:
-#     db.profile.insert_one({"mobile":phone_number})
